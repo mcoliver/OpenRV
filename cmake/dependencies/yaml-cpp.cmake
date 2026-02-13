6be9a05c7337c707cf37dec0b3c449a7fe751c80
Michael Oliver <mcoliver@gmail.com>
2026-02-13 10:54:29 -0800

    yaml-cpp was causing build issues.  Try and find and use it first
    fixes for yaml-cpp build errors
    
    Signed-off-by: Michael Oliver <mcoliver@gmail.com>

--- cmake/dependencies/yaml-cpp.cmake
+++ cmake/dependencies/yaml-cpp.cmake
@@ -9,49 +9,73 @@
 # Expose OCIO's own 'yaml_cpp' target replacing the legacy 'src/pub/yaml_cpp' folder.
 #
 # ##############################################################################################################################################################
-ADD_LIBRARY(yaml_cpp STATIC IMPORTED GLOBAL)
-ADD_DEPENDENCIES(yaml_cpp RV_DEPS_OCIO)
-IF(CMAKE_BUILD_TYPE MATCHES "^Debug$")
-  # Here the postfix is "d" and not "_d": the postfix inside OCIO is: "d".
-  SET(_debug_postfix
-      "d"
+
+# Try to find system yaml-cpp first, as OCIO might have used it.
+FIND_PACKAGE(yaml-cpp QUIET)
+
+IF(yaml-cpp_FOUND)
+  MESSAGE(STATUS "Found system yaml-cpp: ${yaml-cpp_VERSION}")
+  ADD_LIBRARY(yaml_cpp INTERFACE IMPORTED GLOBAL)
+  TARGET_LINK_LIBRARIES(
+    yaml_cpp
+    INTERFACE yaml-cpp::yaml-cpp
   )
-  MESSAGE(DEBUG "Using debug postfix: '${_debug_postfix}'")
+
+  # Still depend on OCIO as it is a major dependency sibling
+  ADD_DEPENDENCIES(yaml_cpp RV_DEPS_OCIO)
+
+  SET(RV_DEPS_YAML_CPP_VERSION
+      "${yaml-cpp_VERSION}"
+      CACHE INTERNAL "" FORCE
+  )
+
 ELSE()
-  SET(_debug_postfix
-      ""
+  MESSAGE(STATUS "System yaml-cpp not found, assuming OCIO internal build.")
+
+  ADD_LIBRARY(yaml_cpp STATIC IMPORTED GLOBAL)
+  ADD_DEPENDENCIES(yaml_cpp RV_DEPS_OCIO)
+  IF(CMAKE_BUILD_TYPE MATCHES "^Debug$")
+    # Here the postfix is "d" and not "_d": the postfix inside OCIO is: "d".
+    SET(_debug_postfix
+        "d"
+    )
+    MESSAGE(DEBUG "Using debug postfix: '${_debug_postfix}'")
+  ELSE()
+    SET(_debug_postfix
+        ""
+    )
+  ENDIF()
+
+  IF(RHEL_VERBOSE)
+    SET(_lib_dir
+        ${RV_DEPS_OCIO_DIST_DIR}/lib64
+    )
+  ELSE()
+    SET(_lib_dir
+        ${RV_DEPS_OCIO_DIST_DIR}/lib
+    )
+  ENDIF()
+
+  SET(_ocio_yaml_cpp_libpath
+      ${_lib_dir}/${CMAKE_STATIC_LIBRARY_PREFIX}yaml-cpp${_debug_postfix}${CMAKE_STATIC_LIBRARY_SUFFIX}
+  )
+  SET_PROPERTY(
+    TARGET yaml_cpp
+    PROPERTY IMPORTED_LOCATION ${_ocio_yaml_cpp_libpath}
+  )
+
+  # It is required to force directory creation at configure time otherwise CMake complains about importing a non-existing path
+  SET(_yaml_cpp_include_dir
+      ${RV_DEPS_OCIO_DIST_DIR}/include
+  )
+  FILE(MAKE_DIRECTORY ${_yaml_cpp_include_dir})
+  TARGET_INCLUDE_DIRECTORIES(
+    yaml_cpp
+    INTERFACE ${_yaml_cpp_include_dir}
+  )
+
+  SET(RV_DEPS_YAML_CPP_VERSION
+      "0.7.0"
+      CACHE INTERNAL "" FORCE
   )
 ENDIF()
-
-IF(RHEL_VERBOSE)
-  SET(_lib_dir
-      ${RV_DEPS_OCIO_DIST_DIR}/lib64
-  )
-ELSE()
-  SET(_lib_dir
-      ${RV_DEPS_OCIO_DIST_DIR}/lib
-  )
-ENDIF()
-
-SET(_ocio_yaml_cpp_libpath
-    ${_lib_dir}/${CMAKE_STATIC_LIBRARY_PREFIX}yaml-cpp${_debug_postfix}${CMAKE_STATIC_LIBRARY_SUFFIX}
-)
-SET_PROPERTY(
-  TARGET yaml_cpp
-  PROPERTY IMPORTED_LOCATION ${_ocio_yaml_cpp_libpath}
-)
-
-# It is required to force directory creation at configure time otherwise CMake complains about importing a non-existing path
-SET(_yaml_cpp_include_dir
-    ${RV_DEPS_OCIO_DIST_DIR}/include
-)
-FILE(MAKE_DIRECTORY ${_yaml_cpp_include_dir})
-TARGET_INCLUDE_DIRECTORIES(
-  yaml_cpp
-  INTERFACE ${_yaml_cpp_include_dir}
-)
-
-SET(RV_DEPS_YAML_CPP_VERSION
-    "0.7.0"
-    CACHE INTERNAL "" FORCE
-)
